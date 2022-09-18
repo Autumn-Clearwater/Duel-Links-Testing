b8e55b44651cb881bf14501a5d3c90e999bfa3e6
Autumn-Clearwater <dimitriwhisenhunt@gmail.com>
2022-09-18 12:04:08 -0500

    pop

--- script/c210000015.lua
+++ script/c210000015.lua
@@ -1,7 +1,7 @@
 --Avenger and Doom Dragon
 function c210000015.initial_effect(c)
 --Activate
-	aux.AddSkillProcedure(c,4,false,nil,nil)
+	aux.AddSkillProcedure(c,43false,nil,nil)
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
--- script/c210000065.lua
+++ script/c210000065.lua
@@ -1,7 +1,7 @@
 --Dark Tuning 100
 function c210000065.initial_effect(c)
 --Activate
-	aux.AddSkillProcedure(c,4,false,nil,nil)
+	aux.AddSkillProcedure(c,3,false,nil,nil)
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
